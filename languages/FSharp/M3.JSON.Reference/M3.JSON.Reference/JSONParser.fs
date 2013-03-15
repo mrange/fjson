2a7c96e7507f44c84b32c7ac025e0abed4e6a958
mrange <marten_range@hotmail.com>
2013-03-15 06:57:09 +0100

    1. Borrowed the old HRON reference parser, will redo into JSON reference parser

---
+++ languages/FSharp/M3.JSON.Reference/M3.JSON.Reference/JSONParser.fs
@@ -0,0 +1,135 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+namespace M3.JSON.Reference
+
+open System
+open System.Diagnostics
+open System.Text
+
+open Parser
+
+type ValueLine      =
+    |   EmptyLine   of string
+    |   CommentLine of int*string
+    |   ContentLine of string          
+
+type Member         =
+    |   Empty   of string
+    |   Value   of string*ValueLine[]
+    |   Object  of string*Member[]
+    |   Comment of int*string
+
+type PreProcessor   =   string
+
+type HRON           = PreProcessor[]*Member[]
+
+module HRONParser =
+
+    let p_preprocessor_tag  = p_char '!'
+    let p_value_tag         = p_char '='
+    let p_object_tag        = p_char '@'
+    let p_comment_tag       = p_char '#'
+
+    let p_empty_string  = p_many (p_whitespace >>! p_eol) >>? (fun cs -> new string(cs)) 
+    let p_string        = p_many (p_any_char >>! p_eol) >>? (fun cs -> new string(cs))
+    let p_any_indention = p_many p_tab >>? (fun cs -> cs.Length)
+    let p_comment_string= p_any_indention
+                            .>> p_comment_tag 
+                            >>  p_string 
+
+    let p_preprocessor  = p_preprocessor_tag
+                            >>. p_string
+                            .>> p_eol
+
+    let p_preprocessors = p_many p_preprocessor                              
+
+    let p_empty_line    = p_empty_string 
+                            .>> p_eol 
+                            >>? EmptyLine
+    let p_comment_line  = p_comment_string
+                            .>> p_eol 
+                            >>? CommentLine
+    let p_nonempty_line = p_indention 
+                            >>. p_string 
+                            .>> p_eol
+                            >>? ContentLine
+
+    let p_value_line    = p_choose [
+                            p_nonempty_line ;
+                            p_comment_line  ; 
+                            p_empty_line    ;
+                            ]
+    let p_value_lines   = p_many (p_value_line >>! p_eos)
+
+    let p_value         = p_indention 
+                            >>. p_value_tag 
+                            >>. p_string 
+                            .>> p_eol 
+                            .>> p_indent 
+                            >>  p_value_lines  
+                            .>> p_dedent >>? Value
+
+    let p_empty         = p_empty_string 
+                            .>> p_eol 
+                            >>? Empty
+
+    let p_comment       = p_comment_string
+                            .>> p_eol 
+                            >>? Comment
+
+    let rec p_members ps= (p_many (p_member >>! p_eos)) ps 
+    and p_object ps     = (p_indention 
+                            >>. p_object_tag 
+                            >>. p_string 
+                            .>> p_eol 
+                            .>> p_indent 
+                            >> p_members  
+                            .>> p_dedent >>? Object
+                            ) ps
+
+    and p_member ps     = (p_choose [
+                            p_value     ; 
+                            p_object    ; 
+                            p_comment   ; 
+                            p_empty     ;
+                            ]) ps
+
+    let p_hron  = p_preprocessors >> p_members
+
+    let to_string (hron : HRON) =
+        let value_to_string (sb : StringBuilder) i v =
+            match v with
+                |   CommentLine (i, ln) -> sb.Append('\t', i).Append('#').Append(ln).AppendLine()
+                |   EmptyLine   ws      -> sb.Append(ws).AppendLine()
+                |   ContentLine ln      -> sb.Append('\t', i).Append(ln).AppendLine()
+        let rec member_to_string (sb : StringBuilder) i m =
+            match m with
+                |   Comment (i, ln)     -> sb.Append('\t', i).Append('#').Append(ln).AppendLine()
+                |   Empty   ws          -> sb.Append(ws).AppendLine()
+                |   Object  (nm, ms)    ->
+                    ignore (sb.Append('\t', i).Append('@').Append(nm).AppendLine())
+                    for m' in ms do
+                       ignore (member_to_string sb (i + 1) m')
+                    sb
+                |   Value (nm, vs)      ->                         
+                    ignore (sb.Append('\t', i).Append('=').Append(nm).AppendLine())
+                    for v' in vs do
+                       ignore (value_to_string sb (i + 1) v')
+                    sb
+        let sb = new StringBuilder(128)
+        let (preprocessors, members) = hron
+        for p' in preprocessors do
+            ignore (sb.Append('!').Append(p').AppendLine())
+        for m' in members do
+            ignore (member_to_string sb 0 m')
+        sb.ToString ()
---
+++ languages/FSharp/M3.JSON.Reference/M3.JSON.Reference/Parser.fs
@@ -0,0 +1,212 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A 
+// copy of the license can be found in the License.html file at the root of this distribution. 
+// If you cannot locate the  Microsoft Public License, please send an email to 
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+namespace M3.JSON.Reference
+
+open System
+open System.Diagnostics
+
+type ParserState    = {input : string; pos : int; indent : int;}
+
+type ParserResult<'a>  =
+    |   Success of 'a*ParserState
+    |   Failure of string*ParserState
+
+type Parser<'a>     = ParserState -> ParserResult<'a>
+
+module Parser =
+    
+    let parse (p : Parser<'a>) s    =
+        let ps = {input = s; pos = 0; indent = 0}
+        p ps
+
+    let advance     ps      = {ps with pos = ps.pos + 1}
+    let move_to     ps pos  = {ps with pos = pos}
+    let is_not_eos  ps      = ps.pos < ps.input.Length
+    let is_eos      ps      = ps.pos >= ps.input.Length
+    let is_success  pr      =   match pr with
+                                    |   Success(_,_)    -> true
+                                    |   _               -> false   
+
+    let p_success v         = (fun ps -> Success(v, ps))
+    
+    let p_failure v         = (fun ps -> Failure(v, ps))
+
+    let current_pos ps      = 
+        let center  = 0
+        let width   = 16
+        let start = max (ps.pos - center) 0
+        let stop = min (ps.pos + width + (start - (ps.pos - center))) ps.input.Length  
+        ps.input.Substring(start, stop - start).Replace("\r", "\\r").Replace("\n", "\\n").Replace("\t", "\\t")
+
+    let p_debug p           = (fun ps -> 
+//        if System.Diagnostics.Debugger.IsAttached then System.Diagnostics.Debugger.Break ()
+        if (is_eos ps) then printf "DEBUG: EOS\r\n"
+        else (printf "DEBUG: pos:%d indent:%d %s\r\n" ps.pos ps.indent (current_pos ps))        
+        let r = p ps
+        r
+        )
+
+    let p_eos               = (fun ps -> if is_eos ps then Success((), ps) else Failure("Expected EOS", ps))
+
+    let p_any_char      = (fun ps -> 
+        if is_not_eos ps
+            then Success (ps.input.[ps.pos], advance ps)
+            else Failure ("EOS", ps)
+        )
+
+    let p_satisy test e = (fun ps -> 
+        if is_not_eos ps
+            then
+                if (test ps.input.[ps.pos]) 
+                    then Success (ps.input.[ps.pos], advance ps)
+                    else Failure (e, ps)
+            else  Failure ("EOS", ps)
+        )
+
+    let p_satisy_many test = (fun ps ->         
+        let mutable p = ps.pos
+        let e = ps.input.Length
+
+        while (p < e) && (test (p - ps.pos) ps.input.[p]) do
+            p <- p + 1
+
+        Success (ps.input.Substring(ps.pos, p - ps.pos), move_to ps p)
+        )
+
+    let p_map (p : Parser<'a>) m = (fun ps ->
+        match p ps with
+            |   Success(v, ps') ->  Success(m v, ps')
+            |   Failure(e, _)   ->  Failure(e,ps)
+        )
+
+    let p_many (p : Parser<'a>)  = 
+        (fun ps ->
+            let mutable is_looking  = true
+            let mutable ps'         = ps
+            let         result      = new ResizeArray<'a> ()
+            while (is_looking) do
+                let pr = p ps'
+                match pr with
+                    |   Success (v, ps'')   ->
+                        result.Add(v)
+                        ps'     <- ps''
+                    |   _       ->
+                        is_looking <- false
+
+            Success (result.ToArray (), ps')
+        )
+
+    let p_opt (p : Parser<'a>)  = (fun ps ->
+        match p ps with
+            |   Success (v, ps')    ->  Success (Some v, ps')
+            |   Failure (_, _)      ->  Success (None, ps)
+        )
+        
+    let p_except (p : Parser<'a>) (pe : Parser<'b>) = (fun ps ->
+        match pe ps with
+            |   Success (_, _)  ->  Failure ("Unexpected", ps)
+            |   Failure (_, _)  ->  p ps
+        )
+
+    let p_choose (parsers : Parser<'a> list) = 
+        (fun ps ->
+            let mutable parsers'    = parsers
+            let mutable ps'         = ps
+            let mutable result      = None
+            while parsers'.Length > 0 do
+                let pr = parsers'.Head ps'
+                match pr with
+                    |   Success (v, ps'') ->
+                        parsers'    <- []
+                        ps'         <- ps''
+                        result      <- Some v
+                    |   _   ->
+                        parsers' <- parsers'.Tail
+
+            match result with
+                |   Some x  -> Success (x, ps')
+                |   _       -> Failure ("No match found", ps)                         
+        )
+
+    let p_combine (pl : Parser<'a>) (pr : Parser<'b>) = (fun ps ->
+        match pl ps with
+            |   Success (l, ps')    ->  
+                match pr ps' with
+                    |   Success (r, ps'')   ->  Success((l,r), ps'')
+                    |   Failure (e, _)      ->  Failure(e, ps)
+            |   Failure (e, _)      ->  Failure (e, ps)
+        )
+
+    let p_keep_left (pl : Parser<'a>) (pr : Parser<'b>) = 
+        let combiner = p_combine pl pr
+        (fun ps ->
+        match combiner ps  with
+            |   Success ((l,_), ps')    ->  Success (l, ps')
+            |   Failure (e, _)          ->  Failure (e, ps)
+        )
+
+    let p_keep_right (pl : Parser<'a>) (pr : Parser<'b>) = 
+        let combiner = p_combine pl pr
+        (fun ps ->
+        match combiner ps  with
+            |   Success ((_,r), ps')    ->  Success (r, ps')
+            |   Failure (e, _)          ->  Failure (e, ps)
+        )
+
+    let p_indent= (fun ps ->
+        Success ((), {ps with indent = ps.indent + 1})
+        )
+
+    let p_dedent= (fun ps ->
+        Success ((), {ps with indent = ps.indent - 1})
+        )
+
+    let (>>)    = p_combine
+    let (.>>)   = p_keep_left
+    let (>>.)   = p_keep_right          
+    let (>>?)   = p_map
+    let (>>!)   = p_except
+
+    let p_char ch               = (p_satisy (fun c -> c = ch) ("Expected: " + ch.ToString())) >>? (fun ch -> ()) 
+    let p_string (str : string) = (p_satisy_many (fun i c -> (i < str.Length) && (str.[i] = c))) >>? (fun str -> ()) 
+
+    let p_whitespace            = p_satisy Char.IsWhiteSpace "Expected whitespace"
+    let p_whitespaces           = p_satisy_many (fun _ c -> Char.IsWhiteSpace(c))
+
+    let p_cr                    = p_choose [p_char '\r'; p_eos;]
+    let p_ln                    = p_choose [p_char '\n'; p_eos;]
+    let p_tab                   = p_char '\t'
+
+    let p_eol = (fun ps ->
+        match p_cr ps with
+            |   Success (_, ps')            -> 
+                match p_ln ps' with
+                    |   Success (_, ps'')   -> Success ((), ps'')
+                    |   Failure (_, _)      -> Success ((), ps')
+            |   Failure (_, _)              -> p_ln ps        
+        )
+
+    let p_indention = 
+        (fun ps ->
+            let s = ps.pos 
+            let e = min ps.input.Length (ps.pos + ps.indent)
+            let mutable r = true
+            for x in s..(e - 1) do
+                r <- (ps.input.[x] = '\t') && r 
+            
+            if r 
+            then    Success ((), move_to ps (ps.pos + ps.indent))
+            else    Failure("Expected indent", ps)    
+        )     
+
+
