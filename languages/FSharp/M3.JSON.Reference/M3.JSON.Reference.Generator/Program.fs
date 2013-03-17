255ba99db01eb131091b9c1dba32a3303e53b7ed
mrange <marten_range@hotmail.com>
2013-03-17 09:17:19 +0100

    1. Bug fixes to the generator

--- languages/FSharp/M3.JSON.Reference/M3.JSON.Reference.Generator/Program.fs
+++ languages/FSharp/M3.JSON.Reference/M3.JSON.Reference.Generator/Program.fs
@@ -19,7 +19,7 @@ open M3.JSON.Reference
 open Parser
 open JSONParser
 
-let rec randomizer max_level current_level max_members (r: Random) (wsg : int->Random->Whitespace) (sg : Random->WSString)=
+let rec randomizer max_level current_level max_members (r: Random) (wsg : int->Random->Whitespace) (sg : int->Random->WSString)=
     let x = if current_level = 0 then r.Next(5,7)
             else if current_level = max_level then r.Next(0,5)
             else r.Next(0,7)
@@ -28,20 +28,23 @@ let rec randomizer max_level current_level max_members (r: Random) (wsg : int->R
         | 1 -> TrueValue (wsg current_level r)
         | 2 -> FalseValue (wsg current_level r)
         | 3 -> NumberValue (r.NextDouble()*1000.0, (wsg current_level r))
-        | 4 -> StringValue (sg r)
+        | 4 -> StringValue (sg current_level r)
         | 5 ->  
                 let vc = r.Next(0,max_members)
                 let vs = [| for i in 1..vc -> randomizer max_level (current_level + 1) max_members r wsg sg |]
                 ArrayValue (vs, (wsg current_level r))
         | 6 ->  
                 let oc = r.Next(0,max_members)
-                let os = [| for i in 1..oc -> ((sg r) , randomizer max_level (current_level + 1) max_members r wsg sg) |]
+                let os = [| for i in 1..oc -> ((sg current_level r) , randomizer max_level (current_level + 1) max_members r wsg sg) |]
                 ObjectValue (os, (wsg current_level r))
         | _ ->  ArrayValue ([||], (wsg current_level r))
 
-let indenter current_level random = ("", "")
+let indenter current_level (random : Random) = ("\r\n" + new String ('\t', current_level), "")
 
-let string_generator random = ("test", ("", ""))
+let string_generator current_level (random : Random) =
+    let c = random.Next(0,32) 
+    let cs = [| for i in 1..c -> char (random.Next(int 'A', int 'Z' + 1)) |]    
+    (new String (cs), indenter current_level random)
 
 [<EntryPoint>]
 let main argv =
